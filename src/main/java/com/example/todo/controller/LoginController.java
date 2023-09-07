c51aa875f1e67102e9a7a225f9d1e785df865f24
k-nagasawa <k-nagasawa>
2023-09-07 10:22:14 +0900

    update: pathやmapping修正

--- src/main/java/com/example/todo/controller/LoginController.java
+++ src/main/java/com/example/todo/controller/LoginController.java
@@ -2,18 +2,43 @@ package com.example.todo.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.todo.dao.UserDao;
+import com.example.todo.repository.UserRepository;
 
 @Controller
 public class LoginController {
+	@Autowired
+    HttpServletRequest httpServletRequest;
+
+	@Autowired
+    UserRepository userRepository;
     
 	@GetMapping(path="login")
 	String login(Model model) {
 		return "/Login";
 	}
+
+	    /** ログイン処理を行う */
+    @PostMapping(path="todoLogin")
+    String todoLogin(@RequestParam String userName, String password, Pbkdf2PasswordEncoder passwordEncoder, Model model){
+        UserDao userDao = userRepository.findByUserNameDao(userName);
+        //パスワードがDBと一致しなかった場合
+        if (!passwordEncoder.matches(password,userDao.getPassword())) {
+            //ログイン画面に戻る
+            return "redirect:/login";
+        }
+        //Sessionにユーザ名を設定
+        httpServletRequest.getSession().setAttribute("userName", userName);
+        return "redirect:todo/todoList";
+    }
 
 	@PostMapping(path = "logout")
 	String logout(HttpServletRequest httpServletRequest){
--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -5,16 +5,15 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.validation.annotation.Validated;
 
 import com.example.todo.dao.TodoDao;
-import com.example.todo.dao.UserDao;
 import com.example.todo.form.TodoForm;
 import com.example.todo.repository.TodoRepository;
 import com.example.todo.repository.UserRepository;
@@ -35,22 +34,8 @@ public class TodoController {
     @Autowired
     TodoService todoService;
 
-    /** ログイン処理を行う */
-    @PostMapping(path="todoLogin")
-    String todoLogin(@RequestParam String userName, String password, Pbkdf2PasswordEncoder passwordEncoder, Model model){
-        UserDao userDao = userRepository.findByUserNameDao(userName);
-        //パスワードがDBと一致しなかった場合
-        if (!passwordEncoder.matches(password,userDao.getPassword())) {
-            //ログイン画面に戻る
-            return "redirect:/login";
-        }
-        //Sessionにユーザ名を設定
-        httpServletRequest.getSession().setAttribute("userName", userName);
-        return todo(model);
-    }
-
     /** TODO管理画面に遷移する */
-    @PostMapping(path="todoList")
+    @GetMapping(path="todoList")
     String todo(Model model) {
         String userName = httpServletRequest.getSession().getAttribute("userName").toString();
         List<TodoDao> todoList = todoRepository.findByuserNameList(userName);
@@ -64,14 +49,14 @@ public class TodoController {
     @PostMapping(path="addTodo")
     String addTodo(@Validated TodoForm todoForm, Model model){
         todoService.addTodo(todoForm);
-        return todo(model);
+        return "redirect:todoList";
     }
 
     /**　TODOを削除する */
     @PostMapping(path = "removeTodo")
     String removeTodo(@RequestParam String id, Model model){
         todoService.removeTodo(id);
-        return todo(model);
+        return "redirect:todoList";
     }
 
     /**　チェックを反映 */
@@ -84,16 +69,16 @@ public class TodoController {
             todoDao.setIsClose(null);
         }
         todoService.updateTodo(id, todoDao);
-        return todo(model);
+        return "redirect:todoList";
     }
 
     /**　TODO編集画面遷移 */
     @PostMapping(path = "editTodo")
     String editTodo(@RequestParam String id, Model model){
         TodoDao todoDao = todoRepository.findById(id).get();
-        String check = "未完了";
-        if (todoDao.getIsClose() == true) {
-            check = "完了";
+        String check = "完了";
+        if (todoDao.getIsClose() == null) {
+            check = "未完了";
         }
         model.addAttribute("todo", todoDao);
         model.addAttribute("check", check);
@@ -108,6 +93,6 @@ public class TodoController {
         todoDao.setTitle(todoForm.getTitle());
         todoDao.setBody(todoForm.getBody());
         todoService.updateTodo(todoForm.getId(), todoDao);
-        return todo(model);
+        return "redirect:todoList";
     }
 }
